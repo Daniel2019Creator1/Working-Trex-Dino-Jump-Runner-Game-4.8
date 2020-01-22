607f31f40eba22d0d033eb3dcbc2dd57f2077d61
Daniel2019Creator1 <57808482+Daniel2019Creator1@users.noreply.github.com>
2020-01-22 18:40:43 -0500

    index.js
    Stuff

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 32) {
+        var box = document.getElementById("messageBox");
+        box.style.visibility="hidden";
+    }
+};
+
+
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
